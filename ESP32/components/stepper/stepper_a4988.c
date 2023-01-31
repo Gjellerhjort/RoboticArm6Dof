e8b282e62caf5dcccc6d016a3ced76ee73995032
Gjellerhjort <mads.gjellerod@gmail.com>
2023-01-31 23:02:05 +0100

    cleaned up a4988 stepper driver code and made in more modular

--- ESP32/components/stepper/stepper_a4988.c
+++ ESP32/components/stepper/stepper_a4988.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "esp_log.h"
 #include <esp_timer.h>
 #include <driver/gpio.h>
@@ -24,18 +25,17 @@ esp_timer_handle_t timer_handle;
 volatile uint16_t INTERVAL_US = 2000;
 
 // definere stepper variables
-volatile uint16_t stepper_1_count = 0; // dette er hvor mange steps stepper 1 skal udfører
-volatile uint8_t stepper_1_direction = 1; // dette er retning motoren skal bevæge sig
-volatile uint16_t stepper_2_count = 0;
-volatile uint8_t stepper_2_direction = 1;
-volatile uint16_t stepper_3_count = 0;
-volatile uint8_t stepper_3_direction = 1;
-volatile uint16_t stepper_4_count = 0;
-volatile uint8_t stepper_4_direction = 1;
-volatile uint16_t stepper_5_count = 0;
-volatile uint8_t stepper_5_direction = 1;
-volatile uint16_t stepper_6_count = 0;
-volatile uint8_t stepper_6_direction = 1;
+#define NUM_STEPPERS 6
+
+typedef struct StepperInfo {
+    uint8_t step_pin;
+    uint8_t dir_pin;
+    volatile uint16_t step_count;
+    volatile uint8_t step_direction;
+    volatile uint16_t min_interval;
+} StepperInfo;
+
+volatile StepperInfo steppers[NUM_STEPPERS];
 
 void call_stepper()
 {
@@ -44,83 +44,19 @@ void call_stepper()
 
 static void stepper_timer_callback(void* arg)
 {
-    // motor 1A
-    if(stepper_1_count > 0){
-        gpio_set_level(STEPPER_1_DIR, stepper_1_direction); // sets DIR arro
-        // makes step 
-        gpio_set_level(STEPPER_1_STEP, 1); // sets STEP pin HIGH
-        gpio_set_level(STEPPER_1_STEP, 0); // sets STEP pin LOW
-        stepper_1_count--;
-    } else
+    for (int i = 0; i < NUM_STEPPERS; i++)
     {
-        gpio_set_level(STEPPER_1_STEP, 0);
-    }
-
-    // motor 2A
-
-    if(stepper_2_count > 0){
-        gpio_set_level(STEPPER_2_DIR, stepper_2_direction);
-        gpio_set_level(STEPPER_2_STEP, 1); //generate step
-        gpio_set_level(STEPPER_2_STEP, 0);
-        stepper_2_count--;
-    } else
-    {
-        gpio_set_level(STEPPER_2_STEP, 0);
-    }
-
-    // motor 3A
-
-    if(stepper_3_count > 0){
-        gpio_set_level(STEPPER_3_DIR, stepper_3_direction);
-        gpio_set_level(STEPPER_3_STEP, 1); //generate step
-        gpio_set_level(STEPPER_3_STEP, 0);
-        stepper_3_count--;
-    } else
-    {
-        gpio_set_level(STEPPER_3_STEP, 0);
-    }
-
-    // motor 4A
-
-    if(stepper_4_count > 0){
-        gpio_set_level(STEPPER_4_DIR, stepper_4_direction);
-        gpio_set_level(STEPPER_4_STEP, 1); //generate step
-        gpio_set_level(STEPPER_4_STEP, 0);
-        stepper_4_count--;
-    } else
-    {
-        gpio_set_level(STEPPER_4_STEP, 0);
-    }
-
-    // motor 5C
-    if(stepper_5_count > 0){
-        gpio_set_level(STEPPER_5_DIR, stepper_5_direction);
-        gpio_set_level(STEPPER_5_STEP, 1); //generate step
-        gpio_set_level(STEPPER_5_STEP, 0);
-        stepper_5_count--;
-    } else
-    {
-        gpio_set_level(STEPPER_5_STEP, 0);
-    }
-
-    // motor 6C
-
-    if(stepper_6_count > 0){
-        gpio_set_level(STEPPER_6_DIR, stepper_6_direction);
-        gpio_set_level(STEPPER_6_STEP, 1); //generate step
-        gpio_set_level(STEPPER_6_STEP, 0);
-        stepper_6_count--;
-    } else
-    {
-        gpio_set_level(STEPPER_6_STEP, 0);
+        if(steppers[i].step_count > 0){
+            gpio_set_level(steppers[i].dir_pin, steppers[i].step_direction); // sets DIR arro
+            // makes step 
+            gpio_set_level(steppers[i].step_pin, 1); // sets STEP pin HIGH
+            gpio_set_level(steppers[i].step_pin, 0); // sets STEP pin LOW
+            steppers[i].step_count--;
+        } else {
+            gpio_set_level(steppers[i].step_pin, 0);
+        }
     }
 }
-
-void stepper_move2000(uint8_t direction)
-    {
-        stepper_1_count = 2000;
-        stepper_1_direction = direction;
-    }
 
 void stepper_setSpeed(uint16_t new_timer_period) 
 {
@@ -134,47 +70,39 @@ void stepper_setSpeed(uint16_t new_timer_period)
 void stepper_moveStep(uint8_t motor_num, uint16_t steps,  uint8_t direction) {
     ESP_LOGI("stepper driver", "steps runing");
     // motor 1A steps
-    if (motor_num == 1) // checker om det er motor 1A
-    { 
-        stepper_1_count = steps; // sætter antal steps som motoren skal bevæge sig  
-        stepper_1_direction = direction; // sætter retning motoren skal bevæge sig
-    } else if (motor_num == 2)  // motor 2A 200
-    { 
-        stepper_2_count = steps;   
-        stepper_2_direction = direction;
-    } else if (motor_num == 3) // motor 3A 200
-    {
-        stepper_3_count = steps;   
-        stepper_3_direction = direction;
-    } else if (motor_num == 4) // motor 4A 200
-    {
-        stepper_4_count = steps;  
-        stepper_4_direction = direction;
-    } else if (motor_num == 5) // motor 5C 200
-    {
-        stepper_5_count = steps;  
-        stepper_5_direction = direction;
-    } else if (motor_num == 6) // motor 6C 200
-    {
-        stepper_6_count = steps;  
-        stepper_6_direction = direction;
-    }
+    steppers[motor_num-1].step_count = steps;
+    steppers[motor_num-1].step_direction = direction;
+}
+
+void stepper_config(uint8_t step_num, uint8_t step_pin, uint8_t dir_pin)
+{
+    steppers[step_num-1].step_pin = step_pin;
+    steppers[step_num-1].dir_pin = dir_pin;
+    steppers[step_num-1].step_count = 0;
+    steppers[step_num-1].step_direction = 1;
+    steppers[step_num-1].min_interval = 2;
 }
 
 void stepper_Init()
 {
+    stepper_config(1, STEPPER_1_STEP, STEPPER_1_DIR);
+    stepper_config(2, STEPPER_2_STEP, STEPPER_2_DIR);
+    stepper_config(3, STEPPER_3_STEP, STEPPER_3_DIR);
+    stepper_config(4, STEPPER_4_STEP, STEPPER_4_DIR);
+    stepper_config(5, STEPPER_5_STEP, STEPPER_5_DIR);
+    stepper_config(6, STEPPER_6_STEP, STEPPER_6_DIR);
+
+
+
     gpio_config_t stepper_gpio_conf;
 
     stepper_gpio_conf.intr_type = GPIO_INTR_DISABLE;
     stepper_gpio_conf.mode = GPIO_MODE_OUTPUT;
-    // configures all step pins
-    stepper_gpio_conf.pin_bit_mask = 
-      (1ULL<<STEPPER_1_DIR) | (1ULL<<STEPPER_4_DIR) 
-    | (1ULL<<STEPPER_2_DIR) | (1ULL<<STEPPER_5_DIR)
-    | (1ULL<<STEPPER_3_DIR) | (1ULL<<STEPPER_6_DIR)
-    |  (1ULL<<STEPPER_1_STEP) | (1ULL<<STEPPER_4_STEP) 
-    | (1ULL<<STEPPER_2_STEP) | (1ULL<<STEPPER_5_STEP)
-    | (1ULL<<STEPPER_3_STEP) | (1ULL<<STEPPER_6_STEP);
+    // configures all step and dir pins
+    for (int i = 0; i < NUM_STEPPERS; i++)
+    {
+        stepper_gpio_conf.pin_bit_mask |= (1ULL<<steppers[i].step_pin) | (1ULL<<steppers[i].dir_pin);
+    }
     stepper_gpio_conf.pull_down_en = 0;
     stepper_gpio_conf.pull_up_en = 0;
 
