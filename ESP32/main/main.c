67eb0a0d79678b4e729bdcd0d56888b881f38473
gjellerhjort <mads.gjellerod@gmail.com>
2023-02-02 08:44:56 +0100

    microstepping working on all motors found a index out of range problem

--- ESP32/main/main.c
+++ ESP32/main/main.c
@@ -16,6 +16,29 @@
 #define LED2_PIN 4
 #define LED3_PIN 25
 
+// definere Stepper Step pin
+#define STEPPER_1_STEP 27 //defining STEP pin of first motor
+#define STEPPER_2_STEP 19    
+#define STEPPER_3_STEP 5
+#define STEPPER_4_STEP 16
+#define STEPPER_5_STEP 0
+#define STEPPER_6_STEP 15
+
+// definere Stepper DIR pin
+#define STEPPER_1_DIR 26 //defining DIR pin of first motor
+#define STEPPER_2_DIR 23
+#define STEPPER_3_DIR 18
+#define STEPPER_4_DIR 17
+#define STEPPER_5_DIR 4
+#define STEPPER_6_DIR 2
+
+#define LED_DELAY 1000
+// MicroStepping
+#define MS1 14
+#define MS2 12
+#define MS3 13
+
+
 #define LED_TIMER_GROUP TIMER_GROUP_0
 #define LED_TIMER_IDX TIMER_0
 #define LED_DELAY 500 // 1 ms
@@ -24,18 +47,22 @@
 void led2_callback(void *arg)
 {
     while (1) {
-        ESP_LOGI("Motor Speed", "Core Id: %d\n", esp_cpu_get_core_id());
-        stepper_setSpeed(2500);
-        vTaskDelay(3000 / portTICK_PERIOD_MS);
-        stepper_setSpeed(1200);
-        vTaskDelay(3000 / portTICK_PERIOD_MS);
+        stepper_moveMicrostep(1, 200, 1, 1);
+        vTaskDelay(6000 / portTICK_PERIOD_MS);
+        stepper_moveMicrostep(1, 200*2, 1, 2);
+        vTaskDelay(6000 / portTICK_PERIOD_MS);
+        stepper_moveMicrostep(1, 200*4, 1, 4);
+        vTaskDelay(6000 / portTICK_PERIOD_MS);
+        stepper_moveMicrostep(1, 200*8, 1, 8);
+        vTaskDelay(6000 / portTICK_PERIOD_MS);
+        stepper_moveMicrostep(1, 200*16, 1, 16);
+        vTaskDelay(6000 / portTICK_PERIOD_MS);
     }
 }
 
 void led3_callback(void *arg)
 {
     while (1) {
-        ESP_LOGI("led3_loop", "Core Id: %d\n", esp_cpu_get_core_id());
         gpio_set_level(LED3_PIN, 1);
         vTaskDelay(LED_DELAY / portTICK_PERIOD_MS*4);
         gpio_set_level(LED3_PIN, 0);
@@ -54,7 +81,12 @@ void app_main() {
     gpio_set_direction(LED2_PIN, GPIO_MODE_OUTPUT);
     gpio_set_direction(LED3_PIN, GPIO_MODE_OUTPUT);
 
+    stepper_config(1, STEPPER_1_STEP, STEPPER_1_DIR);
+    microstepping_config(MS1, MS2, MS3);
     stepper_Init();
+    
+    void stepper_moveMicrostep(uint8_t motor_num, uint16_t steps,  uint8_t direction, uint8_t stepping_resolution);
+
 
     xTaskCreatePinnedToCore(
         led2_callback, // function call name
